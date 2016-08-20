6ef6d1cee5588304bcae93b0309b6d10c616607b
Sato Daisuke <densuke@fuga.jp>
2016-08-20 12:58:17 +0900

    added tool

---
+++ mkdata.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -x
+if [ "x$1" = "x" ]; then
+ echo "usage: $0 URL"
+fi
+curl "$1" |grep 'a href.*http://' -i | egrep -o 'http://[^"]+'| cut -d/ -f3 | sort -u | sed $(awk '{printf("-e /^.*" $1 "$/d ");}' ~/work/squid-bwlist/blackhost.txt  )    |tee /tmp/href.txt
+
